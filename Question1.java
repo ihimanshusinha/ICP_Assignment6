b42e1402e44a11972dcfa856723a13098fc7a4a6
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-01-30 14:02:46 +0530

    Add files via upload

--- Question1.java
+++ Question1.java
@@ -35,16 +35,48 @@ public class Question1 {
        int c=sc.nextInt();
        System.out.print("Enter The Value : ");
        int d=sc.nextInt();
-     System.out.println("Addition is "+ additionSimple(a, b));
-     System.out.println("Subtraction is "+ subtractionSimple(a, b));
-     System.out.println("Multiplication is "+ multiplicationSimple(a, b));
-     System.out.println("Division is "+ divisionSimple(a, b));
-     System.out.println("Remainder is "+remainderSimple(c, d));
-  if(c >=0){
-    System.out.println("Square root is "+squareRootSimple(c));
-  }else{
-    System.out.println("Cannot Find Negative Number Square root  ");
-  }
-  
+ 
+       System.out.println("""
+        Enter your choice to perform the task  :- 
+        1 for addition
+        2 for subtraction
+        3 for multiplication
+        4 for  division
+        5 for Remainder
+        6 for SquareRoot
+               """);
+               int ch=sc.nextInt();
+               if (ch>6) {
+                System.out.println("Invalid Input ");
+               } else {
+                switch (ch) {
+                    case 1:
+                    System.out.println("Addition is "+ additionSimple(a, b));
+                        break;
+                        case 2:
+                        System.out.println("Subtraction is "+ subtractionSimple(a, b));
+                        break;
+                        case 3:
+                        System.out.println("Multiplication is "+ multiplicationSimple(a, b));
+                        break;
+                        case 4:
+                        System.out.println("Division is "+ divisionSimple(a, b));
+                        break;
+                        case 5:
+                        System.out.println("Remainder is "+remainderSimple(c, d));
+                        break;
+                        case 6:
+                        if(c >=0){
+                            System.out.println("Square root is "+squareRootSimple(c));
+                          }else{
+                            System.out.println("Cannot Find Negative Number Square root  ");
+                          }
+                        break;
+                
+                    default:
+                        break;
+                }
+               }
+
     }
 }
